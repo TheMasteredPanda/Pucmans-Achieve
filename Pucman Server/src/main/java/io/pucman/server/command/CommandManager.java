680a02246c7c3f870c35e973bdd537d18f33e96d
root <themasteredpanda@gmail.com>
2017-10-28 14:20:28 +0100

    Rewrote most of the CommandManger in Pucmans Server Library.

--- Pucman Server/src/main/java/io/pucman/server/command/CommandManager.java
+++ Pucman Server/src/main/java/io/pucman/server/command/CommandManager.java
@@ -6,6 +6,8 @@ import io.pucman.common.reflect.ReflectUtil;
 import io.pucman.common.reflect.accessors.FieldAccessor;
 import io.pucman.server.PLibrary;
 import io.pucman.server.manager.Manager;
+import lombok.AccessLevel;
+import lombok.Getter;
 import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.plugin.SimplePluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -17,13 +19,13 @@ import java.util.concurrent.Executors;
  * Manager for PucmanCommands.
  * @param <P>
  */
-//TODO REDO
 public class CommandManager<P extends JavaPlugin> extends Manager<PLibrary>
 {
     /**
      * Executor service used for execution various bodies of the PucmanCommand wrapper.
      */
-    protected ListeningExecutorService service;
+    @Getter(value = AccessLevel.PROTECTED)
+    private ListeningExecutorService service;
 
     public CommandManager(PLibrary instance)
     {
@@ -37,6 +39,6 @@ public class CommandManager<P extends JavaPlugin> extends Manager<PLibrary>
      */
     public void register(P instance, PucmanCommand... commands)
     {
-        Arrays.stream(commands).forEachOrdered(cmd -> instance.getCommand(cmd.getMainAlias()).setExecutor(cmd);
+        Arrays.stream(commands).forEachOrdered(cmd -> instance.getCommand(cmd.getMainAlias()).setExecutor(cmd));
     }
 }
