bfaa89c8851870191d4e6fe7198d8426f81f5119
TheMasteredPanda <themasteredpanda@gmail.com>
2018-01-28 08:31:39 +0000

    Added PlayerWrapper.

---
+++ Pucman Bungee/src/main/java/io/pucman/bungee/player/PlayerWrapper.java
@@ -0,0 +1,23 @@
+package io.pucman.bungee.player;
+
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+import java.lang.ref.SoftReference;
+
+/**
+ * Template class for wrapping the player instance.
+ */
+public class PlayerWrapper
+{
+    private SoftReference<ProxiedPlayer> player;
+
+    public PlayerWrapper(ProxiedPlayer player)
+    {
+        this.player = new SoftReference<>(player);
+    }
+
+    public ProxiedPlayer get()
+    {
+        return player.get();
+    }
+}
